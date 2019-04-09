dcff4ac5956558e3763e738c13ebef524f06ef87
Matt Jarvis <47508694+aequalsplay@users.noreply.github.com>
2019-04-09 15:10:17 +0100

    Creatures in Array

--- sketch.js
+++ sketch.js
@@ -1,19 +1,23 @@
 // var scaler = 10;
 var c0;
 var c1;
+let creatures = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noFill();
   stroke(255, 120);
-  c0 = new Creature(width/4, height/2, 0.5);
-  c1 = new Creature(3 * width/4, height/2, 2);
+  for (var i = 0; i < 5; i++) {
+  creatures.push(new Creature(width/4 * i, height/2, 2));
+  // c1 = new Creature(3 * width/4, height/2, 2);
+}
 }
 
 function draw() {
   background(0);
-
-  c0.display();
-  c1.display();
+  for (var i = 0; i < creatures.length; i++) {
+  creatures[i].display();
+}
+  // c1.display();
 
 }
